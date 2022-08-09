24708093bd1cc0601ad217d751100bdb389191d1
hyannconsulting <hyannconsulting@gmail.com>
2022-08-10 01:30:03 +0200

    delete projets

---
+++ ProjetStagiaireCRM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetStagiaireCRM.Startup))]
+namespace ProjetStagiaireCRM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
