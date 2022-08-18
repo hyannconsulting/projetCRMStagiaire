8960424fe5c53ef8279c8dc5648556f112f35307
hyannconsulting <hyannconsulting@gmail.com>
2022-08-19 01:22:38 +0200

    Add controller +  Activite Sportives + Inscription

---
+++ ProjetCRMStagiaire.Core/Controllers/ActiviteSportivesController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjetCRMStagiaire.Core.Data;
+
+namespace ProjetCRMStagiaire.Core.Controllers
+{
+    public class ActiviteSportivesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ActiviteSportivesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ActiviteSportives
+        public async Task<IActionResult> Index()
+        {
+              return _context.ActiviteSportives != null ? 
+                          View(await _context.ActiviteSportives.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.ActiviteSportives'  is null.");
+        }
+
+        // GET: ActiviteSportives/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.ActiviteSportives == null)
+            {
+                return NotFound();
+            }
+
+            var activiteSportive = await _context.ActiviteSportives
+                .FirstOrDefaultAsync(m => m.ActiviteSportiveId == id);
+            if (activiteSportive == null)
+            {
+                return NotFound();
+            }
+
+            return View(activiteSportive);
+        }
+
+        // GET: ActiviteSportives/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ActiviteSportives/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ActiviteSportiveId,Nom,Description,NombreDePlace,Lieu")] ActiviteSportive activiteSportive)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(activiteSportive);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(activiteSportive);
+        }
+
+        // GET: ActiviteSportives/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.ActiviteSportives == null)
+            {
+                return NotFound();
+            }
+
+            var activiteSportive = await _context.ActiviteSportives.FindAsync(id);
+            if (activiteSportive == null)
+            {
+                return NotFound();
+            }
+            return View(activiteSportive);
+        }
+
+        // POST: ActiviteSportives/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ActiviteSportiveId,Nom,Description,NombreDePlace,Lieu")] ActiviteSportive activiteSportive)
+        {
+            if (id != activiteSportive.ActiviteSportiveId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(activiteSportive);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ActiviteSportiveExists(activiteSportive.ActiviteSportiveId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(activiteSportive);
+        }
+
+        // GET: ActiviteSportives/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.ActiviteSportives == null)
+            {
+                return NotFound();
+            }
+
+            var activiteSportive = await _context.ActiviteSportives
+                .FirstOrDefaultAsync(m => m.ActiviteSportiveId == id);
+            if (activiteSportive == null)
+            {
+                return NotFound();
+            }
+
+            return View(activiteSportive);
+        }
+
+        // POST: ActiviteSportives/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.ActiviteSportives == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.ActiviteSportives'  is null.");
+            }
+            var activiteSportive = await _context.ActiviteSportives.FindAsync(id);
+            if (activiteSportive != null)
+            {
+                _context.ActiviteSportives.Remove(activiteSportive);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ActiviteSportiveExists(int id)
+        {
+          return (_context.ActiviteSportives?.Any(e => e.ActiviteSportiveId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ ProjetCRMStagiaire.Core/Controllers/InscriptionsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjetCRMStagiaire.Core.Data;
+
+namespace ProjetCRMStagiaire.Core.Controllers
+{
+    public class InscriptionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InscriptionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Inscriptions
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Inscriptions.Include(i => i.Stagiaire);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Inscriptions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Inscriptions == null)
+            {
+                return NotFound();
+            }
+
+            var inscription = await _context.Inscriptions
+                .Include(i => i.Stagiaire)
+                .FirstOrDefaultAsync(m => m.InscriptionId == id);
+            if (inscription == null)
+            {
+                return NotFound();
+            }
+
+            return View(inscription);
+        }
+
+        // GET: Inscriptions/Create
+        public IActionResult Create()
+        {
+            ViewData["StagiaireId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Inscriptions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("InscriptionId,IdActiviteSportive,StagiaireId,DateInscription")] Inscription inscription)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(inscription);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StagiaireId"] = new SelectList(_context.Users, "Id", "Id", inscription.StagiaireId);
+            return View(inscription);
+        }
+
+        // GET: Inscriptions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Inscriptions == null)
+            {
+                return NotFound();
+            }
+
+            var inscription = await _context.Inscriptions.FindAsync(id);
+            if (inscription == null)
+            {
+                return NotFound();
+            }
+            ViewData["StagiaireId"] = new SelectList(_context.Users, "Id", "Id", inscription.StagiaireId);
+            return View(inscription);
+        }
+
+        // POST: Inscriptions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("InscriptionId,IdActiviteSportive,StagiaireId,DateInscription")] Inscription inscription)
+        {
+            if (id != inscription.InscriptionId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(inscription);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InscriptionExists(inscription.InscriptionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StagiaireId"] = new SelectList(_context.Users, "Id", "Id", inscription.StagiaireId);
+            return View(inscription);
+        }
+
+        // GET: Inscriptions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Inscriptions == null)
+            {
+                return NotFound();
+            }
+
+            var inscription = await _context.Inscriptions
+                .Include(i => i.Stagiaire)
+                .FirstOrDefaultAsync(m => m.InscriptionId == id);
+            if (inscription == null)
+            {
+                return NotFound();
+            }
+
+            return View(inscription);
+        }
+
+        // POST: Inscriptions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Inscriptions == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Inscriptions'  is null.");
+            }
+            var inscription = await _context.Inscriptions.FindAsync(id);
+            if (inscription != null)
+            {
+                _context.Inscriptions.Remove(inscription);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InscriptionExists(int id)
+        {
+          return (_context.Inscriptions?.Any(e => e.InscriptionId == id)).GetValueOrDefault();
+        }
+    }
+}
